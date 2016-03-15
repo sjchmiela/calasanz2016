9eeb01ed2db589c45ea0c372c19f3bd61d1f2f75
Stanisław Chmiela <sjchmiela@gmail.com>
2016-03-16 00:29:20 +0100

    Add new page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
   $('a.tile').nivoLightbox();
   $('.js-hide').css('display', 'none');
+  $('.sdm-registration-form input').change(function(e) {
+    console.log(e);
+    console.log(this);
+  });
 });
 
 if (window.addEventListener) {
   window.addEventListener('load', WHCheckCookies, false);
-} 
+}
 else if (window.attachEvent) {
   window.attachEvent('onload', WHCheckCookies);
 }
