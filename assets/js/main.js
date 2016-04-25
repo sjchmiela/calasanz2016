c3701783d19020000cdaff8ee06d54039d54134f
Stanisław Chmiela <sjchmiela@gmail.com>
2016-04-25 10:12:16 +0200

    Fix problems and pass birthday to form

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
       wyd_beginning = moment('2016-07-25', 'YYYY-MM-DD');
       var age = wyd_beginning.diff(birthdate, 'years');
       $('#age span').text(age);
+      document.birthdate_moment = birthdate;
       if(birthdate.isBefore('1986-07-25')) {
         updateRegistrationForm('birthdate', 'old');
       } else if(birthdate.isSameOrBefore('1998-07-25')) {
@@ -67,22 +68,22 @@ var groupFormLinks = {
     'alumnus': "https://docs.google.com/forms/d/1zNuImq_kgxhtF6sL6j9H3-5NSrBKN5BVRpsU0CnehWw/viewform?entry.2073369867=",
     'connected': "https://docs.google.com/forms/d/10HvkW2NBMg-QcZH19fnL_8WHj8SJ7TphS4TT_TglkVQ/viewform?entry.2073369867="
   },
-  "info21": {
+  "#info21": {
     'student': "https://docs.google.com/forms/d/1jCJwQneRklp08-H-Dg8FgjuezzjOLpyYHYIk-PsnCIo/viewform?entry.2073369867=",
     'alumnus': "https://docs.google.com/forms/d/1nz0AtaFcZ6Lj_gDKnDrKkuXwcCO3oRPizfYpqQiPMaM/viewform?entry.2073369867=",
     'connected': "https://docs.google.com/forms/d/1_S-BCTLZkXhG2CYMpLKxOerXlfRkvTup0r2nmI-Z_Qc/viewform?entry.2073369867="
   },
-  "info22": {
+  "#info22": {
     'student': "https://docs.google.com/forms/d/1C3BuUm3sVFu7t0SOE8f47LwM03bb6ebh7y6GaZQoG7A/viewform?entry.1601380937=",
     'alumnus': "https://docs.google.com/forms/d/1hBiJjCve4j7MR0X9p8eTSyAnBOhf-AmNl9gMe2lEMUo/viewform?entry.654144946="
   },
-  "info23": {
+  "#info23": {
     'student': "https://docs.google.com/forms/d/1q5Tf5AVS3KLqPULembh8J0C0EfoPrhNt4A4peepRIlg/viewform?entry.2073369867=",
     'alumnus': "https://docs.google.com/forms/d/1hSrjl97H_a-cY3mOe-v6T-uR1pOLNOedtWpVEwR50rw/viewform?entry.2073369867="
   }
 };
 
-function group(identifier, student = false, alumnus = false, connected = false) {
+function group(identifier, student, alumnus, connected) {
   $(identifier).show();
   var link_href = "";
   if (student) {
@@ -92,7 +93,7 @@ function group(identifier, student = false, alumnus = false, connected = false) 
   } else if (connected) {
     link_href = groupFormLinks[identifier]['connected'];
   }
-  $(identifier + " .form_link").attr('href', link_href);
+  $(identifier + " .form_link").attr('href', link_href + document.birthdate_moment.format("YYYY-MM-DD"));
   $(identifier + " h4").text("Opcja rejestracji dla Ciebie");
 }
 
@@ -140,14 +141,14 @@ function updateRegistrationForm(field, val) {
     if(newState[q[3]] === 'immature') {
       if(newState[q[4]] === true) {
         // infoA2nocleg1
-        group('#infoA2nocleg1', newState[q[0]], , newState[q[1]], newState[q[2]]);
+        group('#infoA2nocleg1', newState[q[0]], newState[q[1]], newState[q[2]]);
       } else if(newState[q[4]] === false) {
         if(newState[q[5]] === 'full') {
           // info22
-          group('#info22', newState[q[0]], , newState[q[1]], newState[q[2]]);
+          group('#info22', newState[q[0]], newState[q[1]], newState[q[2]]);
         } else if (newState[q[5]] === 'part') {
           // info23
-          group('#info23', newState[q[0]], , newState[q[1]], newState[q[2]]);
+          group('#info23', newState[q[0]], newState[q[1]], newState[q[2]]);
         } else {
           // ask participation
           ask(q[5]);
@@ -158,14 +159,14 @@ function updateRegistrationForm(field, val) {
     } else if(newState[q[3]] === 'mature') {
       if(newState[q[4]] === true) {
         // infoA2nocleg2
-        group('#infoA2nocleg2', newState[q[0]], , newState[q[1]], newState[q[2]]);
+        group('#infoA2nocleg2', newState[q[0]], newState[q[1]], newState[q[2]]);
       } else if(newState[q[4]] === false) {
         if(newState[q[5]] === 'full') {
           // info20
-          group('#info20', newState[q[0]], , newState[q[1]], newState[q[2]]);
+          group('#info20', newState[q[0]], newState[q[1]], newState[q[2]]);
         } else if (newState[q[5]] === 'part') {
           // info21
-          group('#info21', newState[q[0]], , newState[q[1]], newState[q[2]]);
+          group('#info21', newState[q[0]], newState[q[1]], newState[q[2]]);
         } else {
           ask(q[5]);
         }
@@ -185,29 +186,31 @@ function updateRegistrationForm(field, val) {
       if(newState[q[3]] === 'immature') {
         if(newState[q[4]] === true) {
           // infoA2nocleg1
-          group('#infoA2nocleg1', newState[q[0]], , newState[q[1]], newState[q[2]]);
+          group('#infoA2nocleg1', newState[q[0]], newState[q[1]], newState[q[2]]);
         } else if(newState[q[4]] === false) {
           if(newState[q[5]] === 'full') {
             // info22
-            group('#info22', newState[q[0]], , newState[q[1]], newState[q[2]]);
+            group('#info22', newState[q[0]], newState[q[1]], newState[q[2]]);
           } else if (newState[q[5]] === 'part') {
             // info23
-            group('#info23', newState[q[0]], , newState[q[1]], newState[q[2]]);
+            group('#info23', newState[q[0]], newState[q[1]], newState[q[2]]);
           } else {
             ask(q[5]);
           }
+        } else {
+          ask(q[4]);
         }
       } else if(newState[q[3]] === 'mature') {
         if(newState[q[4]] === true) {
           // infoA2nocleg2
-          group('#infoA2nocleg2', newState[q[0]], , newState[q[1]], newState[q[2]]);
+          group('#infoA2nocleg2', newState[q[0]], newState[q[1]], newState[q[2]]);
         } else if(newState[q[4]] === false) {
           if(newState[q[5]] === 'full') {
             // info20
-            group('#info20', newState[q[0]], , newState[q[1]], newState[q[2]]);
+            group('#info20', newState[q[0]], newState[q[1]], newState[q[2]]);
           } else if (newState[q[5]] === 'part') {
             // info21
-            group('#info21', newState[q[0]], , newState[q[1]], newState[q[2]]);
+            group('#info21', newState[q[0]], newState[q[1]], newState[q[2]]);
           } else {
             ask(q[5]);
           }
@@ -229,14 +232,14 @@ function updateRegistrationForm(field, val) {
         } else if(newState[q[3]] === 'mature') {
           if(newState[q[4]] === true) {
             // infoA2nocleg2
-            group('#infoA2nocleg2', newState[q[0]], , newState[q[1]], newState[q[2]]);
+            group('#infoA2nocleg2', newState[q[0]], newState[q[1]], newState[q[2]]);
           } else if(newState[q[4]] === false) {
             if(newState[q[5]] === 'full') {
               // info20
-              group('#info20', newState[q[0]], , newState[q[1]], newState[q[2]]);
+              group('#info20', newState[q[0]], newState[q[1]], newState[q[2]]);
             } else if (newState[q[5]] === 'part') {
               // info21
-              group('#info21', newState[q[0]], , newState[q[1]], newState[q[2]]);
+              group('#info21', newState[q[0]], newState[q[1]], newState[q[2]]);
             } else {
               ask(q[5]);
             }
